696f648ecf94a7954a9046352c8e7f6a016f4694
YaroslavHuryk <yaroslavhuryk63@gmail.com>
2026-04-14 20:50:06 +0300

    cheaged source for image

--- src/Skills.jsx
+++ src/Skills.jsx
@@ -4,12 +4,12 @@ function Skills() {
         <div className="w-full mt-16 h-auto md:pt-24 md:mt-0">
             <h2 className="text-2xl md:text-4xl text-center font-bold text-opacity-90 text-white mb-6 md:mb-12">Skills</h2>
             <div className="grid grid-cols-2 mb-24 gap-12 md:grid-cols-6">
-                <SkillBlock skillName="Git" skillImage="/public/git.png" />
-                <SkillBlock skillName="React" skillImage="/public/react.png" />
-                <SkillBlock skillName="CSS" skillImage="/public/css.png" />
-                <SkillBlock skillName="HTML" skillImage="/public/html.png" />
-                <SkillBlock skillName="JavaScript" skillImage="/public/js.png" />
-                <SkillBlock skillName="Tailwind CSS" skillImage="/public/tailwind.png" />
+                <SkillBlock skillName="Git" skillImage="/git.png" />
+                <SkillBlock skillName="React" skillImage="/react.png" />
+                <SkillBlock skillName="CSS" skillImage="/css.png" />
+                <SkillBlock skillName="HTML" skillImage="/html.png" />
+                <SkillBlock skillName="JavaScript" skillImage="/js.png" />
+                <SkillBlock skillName="Tailwind CSS" skillImage="/tailwind.png" />
             </div>
         </div>
     )

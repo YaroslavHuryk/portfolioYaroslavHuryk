bff0a63c6b9acef720863fc23af1be9adea2b24f
YaroslavHuryk <yaroslavhuryk63@gmail.com>
2026-04-27 22:00:47 +0300

    changed padding in blog block

--- src/Post.jsx
+++ src/Post.jsx
@@ -1,6 +1,6 @@
 function Post({ title, description, imageUrl, date }) {
     return (
-        <div className="h-min-64 h-fit pt-15 w-full md:p-20 p-10 bg-orange-500 bg-opacity-50 rounded-lg">
+        <div className="h-min-64 h-fit pt-10 w-full md:p-20 p-5 bg-orange-500 bg-opacity-50 rounded-lg">
             <img src={imageUrl} alt={title} className="mx-auto mb-4 max-h-96 float-left rounded-xl mr-10" />
             <h2 className="text-gray-200 mt-4 font-bold">{title}</h2>
             <p className="text-gray-300 mt-2">{description}</p>
